a2db024d94291b4d500d9123d9ff4a318cf2a9b9
sledro <dan@sledro.com>
2017-11-16 15:49:29 +0000

    Initial Commit

---
+++ Grid.cpp
@@ -0,0 +1,7 @@
+// Authors: Daniel Hayden & Frank Rooney
+// Created:15/11/17
+
+int main(void){
+
+  return 0;
+}
---
+++ Grid.h
@@ -0,0 +1,12 @@
+// Authors: Daniel Hayden & Frank Rooney
+// Created:15/11/17
+
+class Grid
+{
+private:
+    unsigned int WIDTH;
+
+public:
+    Grid();
+
+};

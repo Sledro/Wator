659006cfd3804150301c92a1fa2012b1991198ac
Sledro <dan@sledro.com>
2017-12-05 13:14:43 +0000

    Fixed issue where fish/sharks were not being displayed in the correct position on the grid.

--- main.cpp
+++ main.cpp
@@ -11,67 +11,74 @@
 #include <iostream>
 using namespace std;
 
-static const int GRID_COLS = 32; //Number of grid columns
-static const int GRID_ROWS = 14; //Number of grid rows
+
+static const int GRID_COLS = 34; //Number of grid columns 34
+static const int GRID_ROWS = 14; //Number of grid rows 14
 
 int nSharks = 10;   //Number of sharks to spawn at launch
-int nFish = 25;     //Number of fish to spawn at launch
+int nFish = 8;     //Number of fish to spawn at launch
 int sBreed = 10;    //Number of time units that pass before a shark can reproduce
 int fBreed = 10;    //Number of time units that pass before a fish can reproduce
 int starve = 10;    //Period of time units a shark can go without food before dying
 int threads = 1;    //Number threads this progrm can create/use
 float chronon = 1.0f;    //Number of seconds per chronon (chronon = unit of time in this ecosystem)
-int FISH[GRID_COLS][GRID_ROWS];   //Location/Age of Fish : -1 = No Fish otherwise number = age
-int SHARKS[GRID_COLS][GRID_ROWS];      //Location/Age of Sharks
+int FISH[GRID_ROWS][GRID_COLS];   //Location/Age of Fish : -1 = No Fish otherwise number = age
+int SHARKS[GRID_ROWS][GRID_COLS];      //Location/Age of Sharks
 
 int main()
 {
 
-    sf::RenderWindow window(sf::VideoMode(1285, 560), "Wator Ecosystem Simulator");
+    sf::RenderWindow window(sf::VideoMode(1360, 560), "Wator Ecosystem Simulator");//1285, 560
 
     Grid grid;      //Create a single grid sprite object
     Fish fish;      //Create a single fish sprite object
     Shark shark;    //Create a single shark sprite object
 
-    sf::Sprite GRID[GRID_COLS][GRID_ROWS];
+    sf::Sprite GRID[GRID_ROWS][GRID_COLS];
     srand (time (0));  //Makes rand() more random
 
     //Fill fish array wih -1's
-    for (int i=0; i<GRID_COLS; i++){
-        for (int j=0; j<GRID_ROWS; j++) {
+    for (int i=0; i<GRID_ROWS; i++){
+        for (int j=0; j<GRID_COLS; j++) {
            FISH[i][j]=-1;
         }
     }
 
     //Enter fish notated by 1's at random locations into the FISH array
     for (int i=0; i<nFish; i++){
-        FISH[rand() % GRID_COLS + 1 ][rand() % GRID_ROWS + 1 ]=1;
+        FISH[rand() % GRID_ROWS + 1 ][rand() % GRID_COLS + 1 ]=1;
     }
 
+
+      //Fill fish array wih -1's
+    for (int i=0; i<GRID_ROWS; i++){
+        for (int j=0; j<GRID_COLS; j++) {
+           std::cout << FISH[i][j];
+        }
+        std::cout << endl;
+    }  
+
     //Fill Shark array wih -1's
-    for (int i=0; i<GRID_COLS; i++){
-        for (int j=0; j<GRID_ROWS; j++) {
+    for (int i=0; i<GRID_ROWS; i++){
+        for (int j=0; j<GRID_COLS; j++) {
            SHARKS[i][j]=-1;
         }
     }
 
     //Enter shark notated by 1's at random locations into the FISH array
     for (int i=0; i<nSharks; i++){
-        SHARKS[rand() % GRID_COLS + 1 ][rand() % GRID_ROWS + 1 ]=1;
+        SHARKS[rand() % GRID_ROWS + 1 ][rand() % GRID_COLS + 1 ]=1;
     }
 
 
     //Fill GRID array with grid sprites
-    for (int i=0; i<GRID_COLS; i++){
-        for (int j=0; j<GRID_ROWS; j++) {
+    for (int i=0; i<GRID_ROWS; i++){
+        for (int j=0; j<GRID_COLS; j++) {
+            if(FISH[i][j]!=1){
             GRID[i][j]=grid.getGridSprite();
-            //put a fish sprite
-            if(FISH[i][j]==1){
+            }else if (FISH[i][j]==1){
                 GRID[i][j]=fish.getFishSprite();
-            }
-            if(SHARKS[i][j]==1){
-                GRID[i][j]=shark.getSharkSprite();
-            }            
+            }       
         }
     }
 
@@ -103,20 +110,27 @@ int main()
         if (timeSinceLastUpdate > timePerFrame)
         {
             window.clear();
-            for (int i=0; i<GRID_COLS; i++){
-                for (int j=0; j<GRID_ROWS; j++) {
+
+            for (int i=0; i<GRID_ROWS; i++){
+                for (int j=0; j<GRID_COLS; j++) {
                     //Set position of sprite to make grid and darw
-                    GRID[i][j].setPosition(i * 40,j * 40);
+                    GRID[i][j].setPosition(j * 40,i * 40);
+
+                   
                     window.draw(GRID[i][j]);
+
+                   // cout << i << " X " << j  << endl;
                 }
+                
+
             }
 
             timeCounter++;
-            std::cout << timeCounter << endl;
+            //std::cout << timeCounter << endl;
             window.display();
 
             //Reset the timeSinceLastUpdate to 0 
-            timeSinceLastUpdate = sf::Time::Zero;
+            //timeSinceLastUpdate = sf::Time::Zero;
         }
     }
 }
